c6e449f7bc60b1896d2cd30da4fa78ada48048b3
Otoya Nakakaze <otoya.nakakaze@rwth-aachen.de>
2022-07-09 22:57:31 +0200

    BLE Indication for routing table is added

--- data_processor/src/ble-wasm.h
+++ data_processor/src/ble-wasm.h
@@ -1,11 +1,3 @@
-#include "esp_bt.h"
-
-#include "esp_gap_ble_api.h"
-#include "esp_gatts_api.h"
-#include "esp_bt_defs.h"
-#include "esp_bt_main.h"
-#include "esp_gatt_common_api.h"
-
 #define GATTS_TAG "BLE-Module"
 
 #define GATTS_SERVICE_UUID_WASM   0x00FF
@@ -13,10 +5,10 @@
 #define GATTS_DESCR_UUID_WASM     0x3333
 #define GATTS_NUM_HANDLE_WASM     4
 
-#define GATTS_SERVICE_UUID_TEST_B   0x00EE
-#define GATTS_CHAR_UUID_TEST_B      0xEE01
-#define GATTS_DESCR_UUID_TEST_B     0x2222
-#define GATTS_NUM_HANDLE_TEST_B     4
+#define GATTS_SERVICE_UUID_MESH_GRAPH   0x00EE
+#define GATTS_CHAR_UUID_MESH_GRAPH      0xEE01
+#define GATTS_DESCR_UUID_MESH_GRAPH     0x2222
+#define GATTS_NUM_HANDLE_MESH_GRAPH     4
 
 #define TEST_DEVICE_NAME            "ESP_GATTS_DEMO"
 #define TEST_MANUFACTURER_DATA_LEN  17
@@ -25,10 +17,9 @@
 
 #define PREPARE_BUF_MAX_SIZE 1024
 
-#define PROFILE_NUM 1
-#define PROFILE_A_APP_ID 0
+#define PROFILE_NUM 2
+#define PROFILE_WASM_APP_ID 0
+#define PROFILE_MESH_GRAPH_APP_ID 1
 
 #define BLE_LOCAL_MTU 500
-
-void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param);
-void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param);
+#define BLE_ROUTE_TABLE_MTU 100
